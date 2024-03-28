0ee76ffdde5e78837760f379e2644d331195de2c
maxmheinze <heinzemax@icloud.com>
2024-03-28 19:21:00 +0100

    included task b replications

--- assignment2/task_b.R
+++ assignment2/task_b.R
@@ -6,7 +6,10 @@ pacman::p_load(
   tmap,
   haven,
   foreign,
-  magrittr
+  magrittr,
+  stargazer,
+  conleyreg,
+  janitor
 )
 
 
@@ -99,3 +102,53 @@ tm_shape(spli) +
             legend.bg.color = "white", 
             frame = FALSE,
             fontfamily = "Lato")
+
+
+
+# Replicate Results -------------------------------------------------------
+
+col1 <- lm(illiteracy ~ distmiss + lati + longi + corr + ita + mis + mis1, data = litr)
+col2 <- lm(illiteracy ~ distmiss + lati + longi + area + tempe + alti + preci + rugg + river + coast + corr + ita + mis + mis1, data = litr)
+
+litr_bra <- subset(litr, country == "BRA")
+col3 <- lm(illiteracy ~ distmiss + lati + longi + as.factor(mesorregi), data = litr_bra)
+col4 <- lm(illiteracy ~ distmiss + lati + longi + area + tempe + alti + preci + rugg + river + coast + as.factor(mesorregi), data = litr_bra)
+
+litr_arg <- subset(litr, country == "Argentina")
+col5 <- lm(illiteracy ~ distmiss + lati + longi + corr, data = litr_arg)
+col6 <- lm(illiteracy ~ distmiss + lati + longi + area + tempe + alti + preci + rugg + river + coast + corr, data = litr_arg)
+
+litr_pry <- subset(litr, country == "Paraguay")
+col7 <- lm(illiteracy ~ distmiss + ita, data = litr_pry)
+col8 <- lm(illiteracy ~ distmiss + area + tempe + alti + preci + rugg + river + coast + ita, data = litr_pry)
+
+stargazer(col1, col2, col3, col4, col5, col6, col7, col8, type = "latex")
+
+
+lit1 <- litr %>%
+  drop_na(lati, longi) %>%
+  mutate(lat = lati,
+         lon = longi)
+
+col1c <- conleyreg(illiteracy ~ distmiss + lati + longi + corr + ita + mis + mis1, data = lit1,
+           dist_cutoff = 11.112, lat = "lat", lon = "lon")
+col2c <- conleyreg(illiteracy ~ distmiss + lati + longi + area + tempe + alti + preci + rugg + river + coast + corr + ita + mis + mis1, data = lit1,
+           dist_cutoff = 11.112, lat = "lat", lon = "lon")
+
+lit1_bra <- subset(lit1, country == "BRA")
+col3c <- conleyreg(illiteracy ~ distmiss + lati + longi + as.factor(mesorregi), data = lit1_bra,
+           dist_cutoff = 11.112, lat = "lat", lon = "lon")
+col4c <- conleyreg(illiteracy ~ distmiss + lati + longi + area + tempe + alti + preci + rugg + river + coast + as.factor(mesorregi), data = lit1_bra,
+           dist_cutoff = 11.112, lat = "lat", lon = "lon")
+
+lit1_arg <- subset(lit1, country == "Argentina")
+col5c <- conleyreg(illiteracy ~ distmiss + lati + longi + corr, data = lit1_arg,
+           dist_cutoff = 11.112, lat = "lat", lon = "lon")
+col6c <- conleyreg(illiteracy ~ distmiss + lati + longi + area + tempe + alti + preci + rugg + river + coast + corr, data = lit1_arg,
+           dist_cutoff = 11.112, lat = "lat", lon = "lon")
+
+lit1_pry <- subset(lit1, country == "Paraguay")
+col7c <- conleyreg(illiteracy ~ distmiss + ita, data =  lit1_pry,
+                   dist_cutoff = 11.112, lat = "lat", lon = "lon")
+col8c <- conleyreg(illiteracy ~ distmiss + area + tempe + alti + preci + rugg + river + coast + ita, data =  lit1_pry,
+                   dist_cutoff = 11.112, lat = "lat", lon = "lon")
